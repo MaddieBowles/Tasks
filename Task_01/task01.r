9b0e3c14350e6bcd36f588c8d2bc27e8a0cca4f8
MaddieBowles <maddiebowles17@gmail.com>
2021-01-27 23:00:38 -0500

    <Madison Bowles> Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,1 +1,41 @@
 # This is a message from Maddie's computer
+#install.packages("swirl")
+
+library(swirl)
+
+swirl()
+Maddie
+
+library (swirl)
+swirl()
+Maddie
+0
+1
+x <- 5+7
+1
+x
+y <- x-3 
+y
+1
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 1000
+1
+1
+1
+my_sqrt <- sqrt(z - 1)
+2
+my_sqrt
+1
+my_div <- z/my_sqrt
+1
+my_div
+1
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+1
+my_div
+2
+0
