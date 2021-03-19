b0bd55c75e0e735372ae8a58981ef5626e715a0e
MaddieBowles <maddiebowles17@gmail.com>
2021-03-19 10:57:56 -0400

    fix

---
+++ Exams/Exam 2.r
@@ -0,0 +1,8 @@
+
+age <- (40:55)
+dnorm1 <- dnorm(age, mean = 30.5, sd = 6.7)
+#Multiply all the numbers in the matrix (% of births) by 7.7
+dnorm1 * 7.7
+#reproductive rate is 7.7 (average of the population)
+
+
